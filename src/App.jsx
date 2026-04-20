31f76b54490ba8d8c8204d9cde3c3d7bf5f72208
Sumukh Bhat <sumukhbhat.dev@gmial.com>
2026-04-20 11:04:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,73 @@
+import {useState, useEffect} from "react";
+import "./App.css"
+function App(){
+  const [todos, setTodos] = useState([]);
+  const [task, setTask] = useState("");
+
+  useEffect(function(){
+    fetch("http://localhost:3000/todos")
+    .then(function(res){
+      return res.json();
+    })
+    .then(function(data){
+      setTodos(data);
+    });
+  },[]);
+
+  function addTodo(){
+    fetch("http://localhost:3000/todos",{
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({task:task})
+    })
+    .then(function(res){return res.json();})
+    .then(function(newTodo){
+      setTodos([...todos,newTodo]);
+      setTask("");
+    });
+  }
+
+  function deleteTodo(id){
+    fetch("http://localhost:3000/todos" + id,{
+      method: "DELETE"
+    })
+    .then(function(){
+      setTodos(todos.filter(function(t){
+        return t._id !== id;
+      }));
+    });
+    
+  }
+
+  return(
+    <div>
+      <h1>My Todo App</h1>
+      <input 
+       value={task}
+       onChange={function(e){
+        setTask(e.target.value);
+       }}
+       placeholder="Enter a task"
+      />
+      <button 
+      onClick={addTodo}
+      >Add</button>
+      <ul>
+        {todos.map(function(todo){
+          return(
+            <li key={todo._id}>
+              {todo.task}
+              <button onClick={
+                function(){
+                  deleteTodo(todo._id);
+                }
+              }>Delete</button>
+            </li>
+          )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
